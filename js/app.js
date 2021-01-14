418bce511637b5ab17465413f6ade89767073906
Kaan Kuscu <33625775+ksckaan1@users.noreply.github.com>
2021-01-14 20:28:38 +0300

    updates

---
+++ js/app.js
@@ -0,0 +1,12 @@
+var logo = document.querySelector(".logo")
+logo.addEventListener('click', function () {
+    location.replace("/")
+})
+
+
+var browse = document.querySelector(".browseButton")
+
+
+browse.addEventListener('click', function () {
+    location.replace("/find.html")
+})
